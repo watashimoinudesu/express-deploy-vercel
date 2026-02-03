a51239279cb2345333a744affec4041717c3d9db
Meena <thanaklung.m@gmail.com>
2026-02-03 21:51:37 +0700

    Set up Express server and Vercel configuration

---
+++ app.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello TechUp!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
