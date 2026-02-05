c73982c2f9db6774eddda5a85ec385d8549bd55d
Meena <thanaklung.m@gmail.com>
2026-02-05 16:03:20 +0700

    Refactor database connection to use environment variable for connection string and enable SSL

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,10 +1,25 @@
-import * as pg from "pg";
-const { Pool } = pg.default;
+// import * as pg from "pg";
+// const { Pool } = pg.default;
+
+// const connectionPool = new Pool({
+//   // ตรงนี้ต้องเปลี่ยน connectionString เป็นของตัวเองด้วยนะ
+//   connectionString:
+//     "postgresql://postgres:Neyoti@4411@db.lnthauphbszppfvboagi.supabase.co:5432/postgres",
+// });
+
+// export default connectionPool;
+
+
+import "dotenv/config";
+import pg from "pg";
+const { Pool } = pg;
 
 const connectionPool = new Pool({
-  // ตรงนี้ต้องเปลี่ยน connectionString เป็นของตัวเองด้วยนะ
-  connectionString:
-    "postgresql://postgres:Neyoti@4411@db.lnthauphbszppfvboagi.supabase.co:5432/postgres",
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  max: 2,
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 10000,
 });
 
 export default connectionPool;
