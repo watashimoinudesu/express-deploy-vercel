897797f41b0057e9ef921d2c115d97feeef3d18c
Meena <thanaklung.m@gmail.com>
2026-02-06 00:19:28 +0700

    feat :posts endpoint to retrieve a single post by ID and handle not found errors

--- app.mjs
+++ app.mjs
@@ -27,15 +27,27 @@ app.get("/env-check", (req, res) => {
 });
 
 
-app.get("/p", async (req, res) => {
+app.get("/posts/:postId", async (req, res) => {
   try {
-    const result = await connectionPool.query("SELECT * FROM posts");
-    return  res.json(result.rows);
-  }catch (error) {
-    console.error("Error getting assignments:", error);
-    return res.status(500).json({ message: "Server could not read table because database connection" })
+    const postId = Number(req.params.postId);
+
+
+    const result = await connectionPool.query(
+      "SELECT * FROM posts WHERE id = $1",
+      [postId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Server could not find a requested post" });
+    }
+
+    return res.status(200).json({ data: result.rows[0] });
+  } catch (error) {
+    console.error("Error getting question by ID:", error);
+    return res.status(500).json({ message: "Server could not read post because database connection" });
   }
 });
+
 
 
 app.get("/health", (req, res) => {
